4816937d039929118140d91d133a06f2b9d816ca
kaito <dankurrxd@gmail.com>
2025-02-13 17:10:40 +0200

    upd

--- inc/header.h
+++ inc/header.h
@@ -11,6 +11,16 @@
 #define DASH_DURATION 0.2f
 #define DASH_COOLDOWN 0.5f
 
+Texture2D idle;
+Texture2D background;
+Texture2D run;
+Texture2D coin;
+
+typedef enum {
+    RIGHT,
+    LEFT
+} e_direction;
+
 typedef enum {
     IDLE,
     RUN,
@@ -18,9 +28,47 @@ typedef enum {
     DASH
 } e_move;
 
-typedef enum {
-    RIGHT,
-    LEFT
-} e_direction;
+typedef enum  {
+    IDLE_A,
+    RUN_A,
+    COIN_A,
+    PICK_UP_A
+} e_animation;
+
+
+typedef struct {
+    Rectangle run_source;
+    Rectangle idle_source;
+    int frame;
+    float running_time;
+    float breathing_time;
+    float breathing_offset;
+    Rectangle coin_source;
+    int coin_frame;
+    float coin_running_time;
+} t_animation_data;
+
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+    float velocity;
+    float speed;
+    e_direction direction;
+    float last_dash_time;
+    t_animation_data animation;
+} t_player;
+
+typedef struct {
+    Vector2 position;
+    Vector2 size;
+    Texture2D texture;
+    float scaleX, scaleY;
+    float grassHeight;
+} Platform;
+
+#define PLATFORM_COUNT 24
+Platform platforms[PLATFORM_COUNT];
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,36 +1,118 @@
 #include "header.h"
 
-Texture2D idle;
-Texture2D background;
-Texture2D run;
-Texture2D idle;
-
-typedef struct {
-    int x;
-    int y;
-    int width;
-    int height;
-    float velocity;
-    float speed;
-    e_direction direction;
-    float last_dash_time;
-} t_player;
-
-bool on_ground(t_player player, int window_height) {
-    if (player.y + player.height >= window_height) {
-        return true;
-    }
-    return false;
+void InitLevel() {
+    platforms[0] = (Platform){{0, 800}, {235, 1}, LoadTexture("resource/textures/platforms/platform6.png"), 0.8f, 0.8f, 15 * 0.8f};
+    platforms[1] = (Platform){{1200, 450}, {235, 1}, LoadTexture("resource/textures/platforms/platform6.png"), 0.8f, 0.8f, 15 * 0.8f};
+    platforms[2] = (Platform){{295, 300}, {235, 1}, LoadTexture("resource/textures/platforms/platform6.png"), 0.8f, 0.8f, 15 * 0.8f};
+    platforms[3] = (Platform){{0, 550}, {130, 1}, LoadTexture("resource/textures/platforms/platform1.png"), 0.7f, 0.7f, 5 * 0.4f};
+    platforms[4] = (Platform){{650, 780}, {130, 1}, LoadTexture("resource/textures/platforms/platform1.png"), 0.7f, 0.7f, 5 * 0.4f};
+    platforms[5] = (Platform){{1450, 170}, {130, 1}, LoadTexture("resource/textures/platforms/platform1.png"), 0.7f, 0.7f, 5 * 0.4f};
+    platforms[6] = (Platform){{300, 750}, {126, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 1.0f, 1.0f, 5 * 0.4f};
+    platforms[7] = (Platform){{323, 520}, {101, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.8f, 0.8f, 5 * 0.4f};
+    platforms[8] = (Platform){{800, 720}, {101, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.8f, 0.8f, 5 * 0.4f};
+    platforms[9] = (Platform){{1280, 120}, {101, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.8f, 0.8f, 5 * 0.4f};
+    platforms[10] = (Platform){{1030, 550}, {90, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.7f, 0.7f, 5 * 0.4f};
+    platforms[11] = (Platform){{920, 520}, {90, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.7f, 0.7f, 5 * 0.4f};
+    platforms[12] = (Platform){{810, 490}, {90, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.7f, 0.7f, 5 * 0.4f};
+    platforms[13] = (Platform){{150, 460}, {160, 1}, LoadTexture("resource/textures/platforms/platform3.png"), 0.7f, 0.7f, 25 * 0.8f};
+    platforms[14] = (Platform){{690, 150}, {160, 1}, LoadTexture("resource/textures/platforms/platform3.png"), 0.7f, 0.7f, 25 * 0.8f};
+    platforms[15] = (Platform){{0, 190}, {160, 10}, LoadTexture("resource/textures/platforms/platform3.png"), 0.7f, 0.7f, 25 * 0.8f};
+    platforms[16] = (Platform){{450, 550}, {255, 1}, LoadTexture("resource/textures/platforms/platform4.png"), 0.7f, 0.7f, 25 * 0.7f};
+    platforms[17] = (Platform){{1250, 250}, {255, 1}, LoadTexture("resource/textures/platforms/platform4.png"), 0.7f, 0.7f, 25 * 0.7f};
+    platforms[18] = (Platform){{950, 185}, {255, 1}, LoadTexture("resource/textures/platforms/platform4.png"), 0.7f, 0.7f, 25 * 0.7f};
+    platforms[19] = (Platform){{1200, 830}, {360, 1}, LoadTexture("resource/textures/platforms/platform4.png"), 1.0f, 1.0f, 25 * 1.0f};
+    platforms[20] = (Platform){{390, 135}, {160, 1}, LoadTexture("resource/textures/platforms/platform5.png"), 0.8f, 0.8f, 25 * 0.95f};
+    platforms[21] = (Platform){{1000, 700}, {160, 1}, LoadTexture("resource/textures/platforms/platform5.png"), 0.8f, 0.8f, 25 * 0.9f};
+    platforms[22] = (Platform){{810, 350}, {290, 1}, LoadTexture("resource/textures/platforms/platform6.png"), 1.0f, 1.0f, 25 * 0.7f};
+    platforms[23] = (Platform){{1180, 660}, {90, 1}, LoadTexture("resource/textures/platforms/platform2.png"), 0.7f, 0.7f, 5 * 0.4f};
 }
 
-t_player *in_window(t_player *player, int window_width) {
-    if(player->x < 0) {
-        player->x = 0;
+void DrawLevel() {
+    for (int i = 0; i < PLATFORM_COUNT; i++) {
+        Rectangle source = {0, 0, platforms[i].texture.width, platforms[i].texture.height};
+        Rectangle dest = {
+            platforms[i].position.x,
+            platforms[i].position.y - platforms[i].grassHeight, // Отрисовываем ниже
+            platforms[i].texture.width * platforms[i].scaleX,
+            platforms[i].texture.height * platforms[i].scaleY
+        };
+        Vector2 origin = {0, 0};
+
+        DrawTexturePro(platforms[i].texture, source, dest, origin, 0, WHITE);
+        DrawRectangleLines(platforms[i].position.x, platforms[i].position.y,
+                   platforms[i].size.x, platforms[i].size.y, RED);
     }
-    if (player->x + player->width > window_width) {
-        player->x = window_width - player->width;
+}
+
+bool CheckCollision(t_player player, Platform plat) {
+    return (player.x < plat.position.x + plat.size.x &&
+            player.x + player.width > plat.position.x &&
+            player.y + player.height > plat.position.y &&
+            player.y + player.height - player.velocity <= plat.position.y);
+}
+
+bool CheckBottomCollision(t_player player, Platform plat) {
+    return (player.x < plat.position.x + plat.size.x &&
+            player.x + player.width > plat.position.x &&
+            player.y < plat.position.y + plat.size.y &&
+            player.y + player.height > plat.position.y + (plat.size.y / 2));
+}
+
+void HandleCollisions(t_player *player, int window_height) {
+    bool onPlatform = false;
+
+    for (int i = 0; i < PLATFORM_COUNT; i++) {
+        if (player->velocity > 0 && CheckCollision(*player, platforms[i])) {
+            player->velocity = 0;
+            player->y = platforms[i].position.y - player->height;
+            onPlatform = true;
+        }
+
+        else if (player->velocity < 0 && CheckBottomCollision(*player, platforms[i])) {
+            player->velocity = 1;
+            player->y = platforms[i].position.y + platforms[i].size.y + 1;
+        }
     }
-    return player;
+
+    if (!onPlatform) {
+        player->velocity += 1;
+    }
+
+    if (player->y + player->height > window_height) {
+        player->y = window_height - player->height;
+        player->velocity = 0;
+    }
+}
+
+void update_animation(t_player *player, e_animation animation, float delta_time) {
+    const float update_time = 1.0f / 12.0f; // Player animation frame rate
+    const float breathing_speed = 7.0f;     // Breathing effect speed
+    const float coin_update_time = 0.1f;    // Coin animation frame rate
+
+    // Update player animation
+    if (animation == RUN_A) {
+        player->animation.running_time += delta_time;
+        if (player->animation.running_time >= update_time) {
+            player->animation.running_time = 0.0f;
+            player->animation.frame = (player->animation.frame + 1) % 6;
+            player->animation.run_source.x = player->animation.frame * (run.width / 6);
+        }
+    } 
+    else if (animation == IDLE_A) {
+        player->animation.breathing_time += delta_time * breathing_speed;
+        player->animation.breathing_offset = sinf(player->animation.breathing_time);  // Breathing effect
+    } 
+    else {
+        player->animation.breathing_offset = 0;
+    }
+
+    // Update coin animation
+    player->animation.coin_running_time += delta_time;
+    if (player->animation.coin_running_time >= coin_update_time) {
+        player->animation.coin_running_time = 0.0f;
+        player->animation.coin_frame = (player->animation.coin_frame + 1) % 14;
+        player->animation.coin_source.x = player->animation.coin_frame * (coin.width / 14);
+    }
 }
 
 int main() {
@@ -45,9 +127,7 @@ int main() {
     background = LoadTexture("resource/textures/background.png");
     run = LoadTexture("resource/textures/run.png");
     idle = LoadTexture("resource/textures/nindzia.png");
-
-    Rectangle run_source = {0.f , 0.f, (float)run.width / 6.f, (float)run.height};
-    Rectangle idle_source = {0.f , 0.f, (float)idle.width, (float)idle.height};
+    coin = LoadTexture("resource/textures/coin.png");
 
     t_player player = {
         .x = window_width / 2,
@@ -57,33 +137,35 @@ int main() {
         .velocity = 3,
         .speed = 3,
         .direction = RIGHT,
-        .last_dash_time = 0
+        .last_dash_time = 0,
+        .animation = {
+            .run_source = {0.f, 0.f, (float)run.width / 6.f, (float)run.height},
+            .idle_source = {0.f, 0.f, (float)idle.width, (float)idle.height},
+            .frame = 0,
+            .running_time = 0.0f,
+            .breathing_time = 0.0f,
+            .breathing_offset = 0.0f,
+
+            // Initialize coin animation data
+            .coin_source = {0, 0, coin.width / 14, coin.height},
+            .coin_frame = 0,
+            .coin_running_time = 0.0f
+        }
     };
 
     e_move move = IDLE;
+    e_animation animation = IDLE_A;
 
-    int frame = 0;
-    float running_time = 0.0f;
-    //float idle_time = 0.0f;
-    const float update_time = 1.0f/12.0f;
-    float breathing_time = 0.0f;
-    const float breathing_speed = 7.0f;
-    float breathing_offset = 0.0f;
+
+    InitLevel();
 
     while (!WindowShouldClose()) {
-
         float delta_time = GetFrameTime();
-        in_window(&player, window_width);
 
         // Jump
-        if (on_ground(player, window_height)) {
-            player.velocity = 0;
-            player.y = window_height - player.height;
-
-            if(IsKeyPressed(KEY_SPACE)) {
-                move = JUMP;
-                player.velocity = -20;
-            }
+        if (player.velocity == 0 && IsKeyPressed(KEY_SPACE)) {
+            move = JUMP;
+            player.velocity = -25;
         }
         player.velocity += gravity;
 
@@ -116,40 +198,27 @@ int main() {
         if (move != DASH) {
             if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
                 move = RUN;
+                animation = RUN_A;
                 player.direction = RIGHT;
                 player.x += player.speed;
-                running_time += delta_time;
-                if (running_time >= update_time) {
-                    running_time = 0.0f;
-                    frame = (frame + 1) % 6;
-                    run_source.x = frame * run.width / 6;
-                }
             }
             else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
                 move = RUN;
+                animation = RUN_A;
                 player.direction = LEFT;
                 player.x -= player.speed;
-                running_time += delta_time;
-                if (running_time >= update_time) {
-                    running_time = 0.0f;
-                    frame = (frame + 1) % 6;
-                    run_source.x = frame * run.width / 6;
-                }
             }
             else {
                 move = IDLE;
+                animation = IDLE_A;
             }
         }
 
-        if (move == IDLE) {
-            breathing_time += delta_time * breathing_speed;
-            breathing_offset = sinf(breathing_time);  // breathing effect
-        } 
-        else {
-            breathing_offset = 0;
-        }
+        update_animation(&player, animation, delta_time);
 
         player.y += player.velocity;
+
+        HandleCollisions(&player, window_height);
 
         BeginDrawing();
 
@@ -162,6 +231,7 @@ int main() {
 
             DrawTexturePro(background, back_src, back_dest, back_origin, rotation, WHITE);
 
+            DrawLevel();
             if (move == DASH) {
                 for (int i = 0; i < 5; i++) {
                     float offset = (i - 2) * 2.0f;
@@ -171,60 +241,47 @@ int main() {
 
             if (move == RUN || move == DASH) {
                 if (player.direction == RIGHT) {
-                    Rectangle src = run_source;
-                    Rectangle dst = (Rectangle){
-                        player.x,
-                        player.y,
-                        player.width,
-                        player.height
+                    DrawTexturePro(run, player.animation.run_source, 
+                        (Rectangle){player.x, player.y, player.width, player.height}, 
+                        (Vector2){0, 0}, 0, WHITE);
+                } 
+                else {
+                    // Flip the texture for left direction
+                    Rectangle flipped_run_source = {
+                        player.animation.run_source.x + player.animation.run_source.width,
+                        player.animation.run_source.y,
+                        -player.animation.run_source.width,
+                        player.animation.run_source.height
                     };
-                    DrawTexturePro(run, src, dst, (Vector2){0, 0}, 0, WHITE);
-                } else {
-                    // smotrit vlevo
-                    Rectangle src = (Rectangle) {
-                        run_source.x + run_source.width,
-                        run_source.y,
-                        -run_source.width,
-                        run_source.height
-                    };
-                    Rectangle dst = (Rectangle) {
-                        player.x,
-                        player.y,
-                        player.width,
-                        player.height
-                    };
-                    DrawTexturePro(run, src, dst, (Vector2){0, 0}, 0, WHITE);
+                    DrawTexturePro(run, flipped_run_source, 
+                        (Rectangle){player.x, player.y, player.width, player.height}, 
+                        (Vector2){0, 0}, 0, WHITE);
                 }
             } 
             if (move == IDLE) {
                 if (player.direction == RIGHT) {
-                    Rectangle src = idle_source;
-                    Rectangle dst = (Rectangle) {
-                        player.x,
-                        player.y - 2 + breathing_offset,
-                        player.width,
-                        player.height
-                    };
-                    DrawTexturePro(idle, src, dst, (Vector2){0, 0}, 0, WHITE);
+                    DrawTexturePro(idle, player.animation.idle_source, 
+                        (Rectangle){player.x, player.y - 2 + player.animation.breathing_offset, player.width, player.height}, 
+                        (Vector2){0, 0}, 0, WHITE);
                 } 
                 else {
-                    // smotrit vlevo
-                    Rectangle src = (Rectangle) {
-                        idle_source.x + idle_source.width,
-                        idle_source.y,
-                        -idle_source.width,
-                        idle_source.height
+                    // Flip the texture for left direction
+                    Rectangle flipped_idle_source = {
+                        player.animation.idle_source.x + player.animation.idle_source.width,
+                        player.animation.idle_source.y,
+                        -player.animation.idle_source.width,
+                        player.animation.idle_source.height
                     };
-                    Rectangle dst = (Rectangle) {
-                        player.x,
-                        player.y - 2 + breathing_offset,
-                        player.width,
-                        player.height
-                    };
-                    DrawTexturePro(idle, src, dst, (Vector2){0, 0}, 0, WHITE);
+                    DrawTexturePro(idle, flipped_idle_source, 
+                        (Rectangle){player.x, player.y - 2 + player.animation.breathing_offset, player.width, player.height}, 
+                        (Vector2){0, 0}, 0, WHITE);
                 }
             }
-        EndDrawing();
+
+            // Draw coin using the updated coin_source
+            DrawTextureRec(coin, player.animation.coin_source, 
+                (Vector2){ platforms[1].position.x + 40, platforms[1].position.y - 27 }, WHITE);
+            EndDrawing();
     }
     
     UnloadTexture(idle);
